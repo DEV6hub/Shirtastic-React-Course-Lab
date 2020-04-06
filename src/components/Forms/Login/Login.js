a8bed3947ac5c0263584a0f42b39fe0634ef66e8
Michael_Vano <michaelv@dev6.com>
2020-04-06 19:07:33 -0400

    styled primary button and some login form

--- src/components/Forms/Login/Login.js
+++ src/components/Forms/Login/Login.js
@@ -2,21 +2,22 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {PrimaryButton} from "../../PrimaryButton/PrimaryButton";
 import FormInput from "../FormInput/FormInput";
+import "./login.css";
 
 
 
 const facebookLogo = require("../../../images/logos/facebook.svg");
 const twitterLogo = require("../../../images/logos/twitter.svg");
 
-export const Login = ({onLoginFn}) => <form onSubmit={(e) => e.preventDefault()}>
+export const Login = ({onLoginFn}) => <form onSubmit={(e) => e.preventDefault()} className="login">
   <h2>Log in</h2>
   <PrimaryButton>
     <img src={facebookLogo} alt={'Login with Facebook'} />
-    with Facebook
+    <span>with Facebook</span>
   </PrimaryButton>
   <PrimaryButton>
     <img src={twitterLogo} alt={'Login with Twitter'} />
-    with Twitter
+    <span>with Twitter</span>
   </PrimaryButton>
   <div>OR</div>
   <FormInput id="login-email-address" label="Email Address" />
