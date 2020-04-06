61fa9866a2a2a30edf233626e4a5734fee293eca
Michael_Vano <michaelv@dev6.com>
2020-04-06 17:56:05 -0400

    setup html for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,27 @@
 import React from "react";
 
+const dev6Logo = require("../../images/logos/dev6logo-white.svg");
+const year = new Date().getUTCFullYear();
 
 const Footer = () => <footer>
-  Footer Here
+  <div>
+    <img src={dev6Logo} alt="Dev 6 Logo" />
+    <div>
+      &copy; {year} Aquent DEV6
+    </div>
+  </div>
+  <div>
+    <div>
+      4 Robert Speck Parkway Suite 240 <br />
+      Mississauga ON L4Z 1S1 <br />
+      Canada
+    </div>
+    <div>
+      <strong>Toll Free:</strong> (866) 464-7790<br />
+      <strong>Office:</strong> (905) 897-7790<br />
+      <strong>Fax:</strong> (905) 897-9952<br />
+    </div>
+  </div>
 </footer>;
 
 export default Footer;
