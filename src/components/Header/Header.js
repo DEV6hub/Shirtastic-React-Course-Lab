da73daddeb368d5d2f46de3e9bcfc281afe78015
Michael_Vano <michaelv@dev6.com>
2020-04-06 17:46:25 -0400

    structured header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,27 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import NavigationButton from "../NavigationButton/NavigationButton";
+import {PrimaryButton} from "../PrimaryButton/PrimaryButton";
 
+const headerLogo = require("../../images/navlogo.png");
+const shoppingCartIcon = require("../../images/icons/icon-basket.svg");
 
 const Header = () => <header>
-  Header here
+  <div>
+    <NavigationButton onClick={() => alert("Nav Button Clicked")} />
+    <img src={headerLogo} alt="Shirtastic Logo - By Aquent Dev 6" />
+  </div>
+  <div>
+    <Link to="/create-shirt">
+      <PrimaryButton>New Design</PrimaryButton>
+    </Link>
+    <button onClick={() => alert("Shopping Cart Button Clicked")}>
+      <img src={shoppingCartIcon} />
+      <div className="total-items">
+        0
+      </div>
+    </button>
+  </div>
 </header>;
 
 export default Header;
