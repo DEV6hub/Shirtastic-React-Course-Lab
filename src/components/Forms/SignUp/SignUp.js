c038a8957fd7d0ddad0818f29b19a0b5761165a7
Michael_Vano <michaelv@dev6.com>
2020-04-07 09:22:42 -0400

    styled step 2 component

--- src/components/Forms/SignUp/SignUp.js
+++ src/components/Forms/SignUp/SignUp.js
@@ -3,6 +3,7 @@ import FormInput from "../FormInput/FormInput";
 import {PrimaryButton} from "../../PrimaryButton/PrimaryButton";
 import Tabs from "../../Tabs/Tabs";
 import "./sign-up.css";
+import {SignUpStep2} from "../SignUpStep2/SignUpStep2";
 
 const tabs = [
   {
@@ -15,16 +16,27 @@ const tabs = [
   },
 ];
 
-export const SignUp = () => <div className="sign-up-component">
-  <h2>Sign up</h2>
+// temporary state until we figure out the context
+// change to see step 2
+const activeTab = tabs[0].id;
+
+export const SignUp = () => <div className={`sign-up-component ${activeTab === tabs[1].id ? 'step2' : ''}`}>
+  { activeTab === tabs[0].id && <h2>Sign up</h2>}
   <div className="tabs-container">
-    <Tabs tabs={tabs} active={tabs[0].id} />
+    <Tabs tabs={tabs} active={activeTab} />
   </div>
-  <form onSubmit={(e) => e.preventDefault()} className="sign-up">
-    <FormInput id="sign-up-email-address" label="Email Address" placeholder="Your Email Address" />
-    <FormInput id="sign-up-password" label="Password" />
-    <FormInput id="sign-up-confirm-password" label="Confirm Password" />
-    <p>By clicking the Sign Up button below, you agree to our Terms of Service and Privacy Policy.</p>
-    <PrimaryButton>Sign Up</PrimaryButton>
-  </form>
+  {
+    activeTab === tabs[0].id &&
+    <form onSubmit={(e) => e.preventDefault()} className="sign-up">
+      <FormInput id="sign-up-email-address" label="Email Address" placeholder="Your Email Address"/>
+      <FormInput id="sign-up-password" label="Password"/>
+      <FormInput id="sign-up-confirm-password" label="Confirm Password"/>
+      <p>By clicking the Sign Up button below, you agree to our Terms of Service and Privacy Policy.</p>
+      <PrimaryButton>Sign Up</PrimaryButton>
+    </form>
+  }
+  {
+    activeTab === tabs[1].id &&
+      <SignUpStep2 />
+  }
 </div>;
--- src/components/Forms/SignUpStep2/SignUpStep2.js
+++ src/components/Forms/SignUpStep2/SignUpStep2.js
@@ -1,5 +1,31 @@
 import React from "react";
+import "./signup-step2.css";
+import FormInput from "../FormInput/FormInput";
+import {PrimaryButton} from "../../PrimaryButton/PrimaryButton";
 
-export const SignUpStep2 = () => <div>
-  Sign Up 2 Component here
+export const SignUpStep2 = () => <div className="step2-component">
+  <h2>Awesome!</h2>
+  <p>Welcome to the club, where can we ship your shirts to? You can always provide this information at checkout.</p>
+  <form className="step2-address">
+    <FormInput id="step2-name" label="Name" placeholder="Johnny Applseed" />
+    <div className="step2-row">
+      <FormInput id="step2-address1" label="Address 1" placeholder="123 Anywhere Ave" />
+      <FormInput id="step2-address2" label="Address 2" placeholder="Suite 101"  />
+    </div>
+    <div className="step2-row">
+      <FormInput id="step2-phone-number" label="Phone Number" placeholder="555-123-1234" />
+      <FormInput id="step2-city" label="City" placeholder="Toronto"  />
+    </div>
+    <div className="step2-row">
+      <FormInput id="step2-country" label="Country" />
+      <div className="step2-row">
+        <FormInput id="step2-province" label="Province" />
+        <FormInput id="step2-postal-code" label="Postal Code" placeholder="L5N 2N5" />
+      </div>
+    </div>
+    <div className="actions">
+      <PrimaryButton>Do This later</PrimaryButton>
+      <PrimaryButton>Save</PrimaryButton>
+    </div>
+  </form>
 </div>;
