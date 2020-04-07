d45d3d0173524a71a6ce8ef7ee7ea8c1ff65a952
Michael_Vano <michaelv@dev6.com>
2020-04-07 11:25:40 -0400

    styled up general catalog page

---
+++ src/constants/shirtColorOptions.js
@@ -0,0 +1,72 @@
+
+const mensPrefix = "MensShirt-";
+const womensPrefix = "WomensShirt-";
+
+export const SHIRT_WHITE = {
+  color: "#ffffff",
+  colorName: "White",
+  menFile: `${mensPrefix}white.jpg`,
+  womenFile: `${womensPrefix}white.jpg`
+};
+
+export const SHIRT_GREY = {
+  color: "#cdcdcd",
+  colorName: "Grey",
+  menFile: `${mensPrefix}grey.jpg`,
+  womenFile: `${womensPrefix}grey.jpg`
+};
+
+export const SHIRT_BLACK = {
+  color: "#444444",
+  colorName: "Black",
+  menFile: `${mensPrefix}black.jpg`,
+  womenFile: `${womensPrefix}black.jpg`
+};
+
+export const SHIRT_BLUE = {
+  color: "#2674a8",
+  colorName: "Blue",
+  menFile: `${mensPrefix}blue.jpg`,
+  womenFile: `${womensPrefix}blue.jpg`
+};
+
+export const SHIRT_GREEN = {
+  color: "#44a264",
+  colorName: "Green",
+  menFile: `${mensPrefix}green.jpg`,
+  womenFile: `${womensPrefix}green.jpg`
+};
+
+export const SHIRT_YELLOW = {
+  color: "#f4da70",
+  colorName: "Yellow",
+  menFile: `${mensPrefix}yellow.jpg`,
+  womenFile: `${womensPrefix}yellow.jpg`
+};
+
+export const SHIRT_PURPLE = {
+  color: "#6e5bd6",
+  colorName: "Purple",
+  menFile: `${mensPrefix}purple.jpg`,
+  womenFile: `${womensPrefix}purple.jpg`
+};
+
+export const SHIRT_RED = {
+  color: "#a7386a",
+  colorName: "Red",
+  menFile: `${mensPrefix}red.jpg`,
+  womenFile: `${womensPrefix}red.jpg`
+};
+
+export const COLOR_OPTIONS = [
+  SHIRT_WHITE,
+  SHIRT_GREY,
+  SHIRT_BLACK,
+  SHIRT_BLUE,
+  SHIRT_GREEN,
+  SHIRT_YELLOW,
+  SHIRT_PURPLE,
+  SHIRT_RED
+];
+
+
--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -1,8 +1,39 @@
 import React from "react";
 import PageLayout from "../../components/PageLayout/PageLayout";
+import "./catalog.css";
+import Tabs from "../../components/Tabs/Tabs";
+
+const catalogTabs = [
+  {
+    id: "all-designs",
+    text: "All Designs"
+  },
+  {
+    id: "mens-shirts",
+    text: "Mens"
+  },
+  {
+    id: "womens-shirts",
+    text: "Womens"
+  },
+];
+
+const selectedTab = catalogTabs[0].id;
+
+const numberOfShirts = 8;
 
 const Catalog = () => <PageLayout>
-  <h1>Catalog</h1>
+  <div className="catalog">
+    <div className="catalog-header">
+      <Tabs tabs={catalogTabs} active={selectedTab} />
+      <div className="shirts-displayed">
+        Showing {numberOfShirts} Designs
+      </div>
+    </div>
+    <div className="catalog-shirts">
+
+    </div>
+  </div>
 </PageLayout>;
 
 export default Catalog;
