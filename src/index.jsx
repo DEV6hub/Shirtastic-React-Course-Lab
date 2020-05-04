90d745046b6e9a9edcab53a6cc79b4e94c23916c
Ali Hamoody <alih@dev6.com>
2020-05-04 11:42:10 -0400

    renaming some js to jsx files

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers';
 import App from './components/App';
 
