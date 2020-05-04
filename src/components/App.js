4328ddbf759081950956363a4e0a22028cf7e4c7
Ali Hamoody <alih@dev6.com>
2020-05-04 11:25:37 -0400

    Converted App class to function component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
@@ -7,20 +7,18 @@ import Catalog from '../pages/Catalog/Catalog';
 import GraphicDisplay from './GraphicDisplay/GraphicDisplay';
 import { StateProvider } from '../state/state';
 
-class App extends Component {
-  render() {
-    return (
-      <StateProvider>
-        <Router>
-          <div className="App">
-            <Route exact path="/" render={() => <Home />} />
-            <Route exact path="/catalog" render={() => <Catalog />} />
-            <Route path="/graphic/:graphicLogo" component={GraphicDisplay} />
-          </div>
-        </Router>
-      </StateProvider>
-    );
-  }
-}
+const App = () => {
+  return (
+    <StateProvider>
+      <Router>
+        <div className="App">
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/catalog" render={() => <Catalog />} />
+          <Route path="/graphic/:graphicLogo" component={GraphicDisplay} />
+        </div>
+      </Router>
+    </StateProvider>
+  );
+};
 
 export default App;
