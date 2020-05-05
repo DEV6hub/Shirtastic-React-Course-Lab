9f7d4e7ec3025d75654ac720070d101996b6e4d8
Ali Hamoody <alih@dev6.com>
2020-05-05 14:00:42 -0400

    Removed old reducers

--- src/state/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import shirts from './shirts';
-import user from './user';
-
-const rootReducer = (state, action) => ({
-  shirts: shirts(state.shirts, action),
-  user: user(state.user, action),
-});
-
-export default rootReducer;
