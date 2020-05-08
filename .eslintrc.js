994b955818e1af0fc1abdb3d84b3325b86eda996
Ali Hamoody <alih@dev6.com>
2020-05-08 09:30:26 -0400

    Turnd off eslint rule for require()

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
   // Add custom rules here
-  rules: {}
+  rules: {
+    'global-require': 'off',
+    'import/no-dynamic-require': 'off',
+  },
 };
