8549ad4c6f535f69ed57664cd4cb38cd56678dc1
Alex Krasavtsev <alexk@dev6.com>
2020-05-15 12:43:28 -0400

    Navigation bar refactoring.

--- src/components/PageLayout/Header/Header.jsx
+++ src/components/PageLayout/Header/Header.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import NavigationButton from '../NavigationButton/NavigationButton';
 import PrimaryButton from '../../PrimaryButton/primary-button';
 import './header.css';
 
-// const headerLogo = require("../../images/navlogo.png");
-// const shoppingCartIcon = require("../../images/icons/icon-basket.svg");
+import headerLogo from '../../../images/navlogo.png';
+import shoppingCartIcon from '../../../images/icon-basket.svg';
 
 const Header = () => (
   <header>
     <div className="left">
-      <Link to="/catalog">{/* <NavigationButton onClick={() => {}} /> */}</Link>
+      <PrimaryButton
+        onClick={() => {
+          console.log('nav click');
+        }}
+      />
       <div className="vertical-line" />
       <Link to="/">
-        {/* <img src={headerLogo} className="logo" alt="Shirtastic Logo - By Aquent Dev 6" /> */}
+        <img src={headerLogo} className="logo" alt="Shirtastic Logo - By Aquent Dev 6" />
       </Link>
     </div>
     <div className="right">
@@ -21,12 +24,14 @@ const Header = () => (
         <PrimaryButton>New Design</PrimaryButton>
       </Link>
       <div className="vertical-line" />
-      {/* <button className="cart-items" onClick={() => alert("Shopping Cart Button Clicked")}>
-      <img className="cart-icon" src={shoppingCartIcon} alt="Icon of a shopping cart" />
-      <div className="total-items">
-        0
-      </div>
-    </button> */}
+      <button
+        type="button"
+        className="cart-items"
+        onClick={() => alert('Shopping Cart Button Clicked')}
+      >
+        <img className="cart-icon" src={shoppingCartIcon} alt="Icon of a shopping cart" />
+        <div className="total-items">0</div>
+      </button>
     </div>
   </header>
 );
