39ca53edca35dabea4ba2ff4deee240eb5cc2bea
Alex Krasavtsev <alexk@dev6.com>
2020-05-19 12:58:42 -0400

    Added page layout prop types

--- src/components/PageLayout/PageLayout.jsx
+++ src/components/PageLayout/PageLayout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import './page-layout.css';
@@ -10,5 +11,13 @@ const PageLayout = ({ children }) => (
     <Footer />
   </>
 );
+
+PageLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+PageLayout.defaultProps = {
+  children: null,
+};
 
 export default PageLayout;
