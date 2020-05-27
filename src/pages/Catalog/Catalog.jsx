82f1a1ed4f369b4340a89d3952347bc65be58f7d
Alex Krasavtsev <alexk@dev6.com>
2020-05-27 16:35:51 -0400

    Implemented new tabs for catalog

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import PageLayout from '../../components/PageLayout/PageLayout';
 import './Catalog.css';
-// import Tabs from '../../components/Tabs/Tabs';
-// import Design from '../../components/Design/Design';
-import CatalogTabs from '../../components/CatalogTabs/CatalogTabs';
+import Tabs from '../../components/Tabs/Tabs';
 
-const Catalog = () => (
-  <PageLayout>
-    <CatalogTabs />
-  </PageLayout>
-);
+// import CatalogTabs from '../../components/CatalogTabs/CatalogTabs';
+
+const catalogTabs = [
+  {
+    id: 'all-designs',
+    label: 'All Designs',
+  },
+  {
+    id: 'mens-shirts',
+    label: 'Mens',
+  },
+  {
+    id: 'womens-shirts',
+    label: 'Womens',
+  },
+];
+
+const selectedTab = catalogTabs[0].id;
+const shirts = 12;
+
+const changeTab = (id) => {
+  console.log('kra tabn', id);
+};
+
+const Catalog = () => {
+  return (
+    <PageLayout>
+      <div className="catalog-wrapper">
+        <div className="catalog-header">
+          <Tabs tabs={catalogTabs} activeTab={selectedTab} onTabClick={changeTab} />
+          <div className="shirts-displayed">Showing {shirts} Designs</div>
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
 
 export default Catalog;
