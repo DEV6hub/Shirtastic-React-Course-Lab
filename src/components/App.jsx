99287eb272897299934a9f687a19c663e156a6f9
Traian Rusu <trusu@customtruck.com>
2020-06-18 15:54:45 -0400

    Connected redux to app. Refactored redux related files to be in their own folder to show more clearly what is part of the redux implementation.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux'
 
 import Home from '../pages/Home/Home';
 import Catalog from '../pages/Catalog/Catalog';
 import Design from '../pages/Design/Design';
 import Graphic from '../pages/Graphic/Graphic';
 import AppProvider from '../state/contexts/AppProvider';
+import store from '../state/redux/store/store'
 
-const App = () => {
-  return (
-    <AppProvider>
-      <Router>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/catalog">
-          <Catalog />
-        </Route>
-        <Route exact path="/design/:shirtId?">
-          <Design />
-        </Route>
-        <Route path="/graphic/:graphicLogo">
-          <Graphic />
-        </Route>
-      </Router>
-    </AppProvider>
+const App = () => (
+    <ReduxProvider store={store}>
+      <AppProvider>
+        <Router>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/catalog">
+            <Catalog />
+          </Route>
+          <Route exact path="/design/:shirtId?">
+            <Design />
+          </Route>
+          <Route path="/graphic/:graphicLogo">
+            <Graphic />
+          </Route>
+        </Router>
+      </AppProvider>
+    </ReduxProvider>
   );
-};
+
 
 export default App;
