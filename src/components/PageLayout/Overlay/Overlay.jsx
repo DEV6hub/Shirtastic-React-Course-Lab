a805e1098220ba8fbe21b981eac9206e41156f0d
Alex Krasavtsev <alexk@dev6.com>
2020-06-19 17:03:23 -0400

    Introduced keyboard ESC handling on shopping cart to close overlay

--- src/components/PageLayout/Overlay/Overlay.jsx
+++ src/components/PageLayout/Overlay/Overlay.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import './overlay.css';
 import { useOverlayContext } from '../../../state/contexts/overlayContext';
 
 const Overlay = ({ children }) => {
-  const { isOpen } = useOverlayContext();
+  const { isOpen, closeOverlay } = useOverlayContext();
+
+  const handleKeyboard = ({ keyCode }) => {
+    if (isOpen) {
+      if (keyCode === 27) {
+        closeOverlay();
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyboard);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyboard);
+    };
+  });
+
   return (
-    <>
-      <div className={`overlay ${!isOpen ? 'closed' : ''}`}>{children}</div>
-    </>
+    <div className="overlay-wrapper">
+      <div className={`overlay ${!isOpen ? 'closed' : ''}`} role="dialog">
+        {children}
+      </div>
+    </div>
   );
 };
 
