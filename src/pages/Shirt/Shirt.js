b359d2ce470f4b142cc8f407680e5cbed8391082
Hasan Ahmad <hasana@dev6.com>
2020-07-05 18:00:08 -0400

    Add shirt object, convert React.createClass to JSX.

--- src/pages/Shirt/Shirt.js
+++ src/pages/Shirt/Shirt.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import './Shirt.css';
 
+const shirt = {
+  id: 1,
+  name: 'Happy Shirt',
+  description: 'Womens Fine Jersey Short Sleeve',
+  price: 14.99,
+  image: 'WomensShirt-blue',
+  quantity: 0,
+  subtotal: 0,
+  graphic: 'graphic12.svg',
+  font: "'Orbitron', sans-serif",
+  text: 'happy little trees',
+  textColor: {
+    name: 'white',
+    color: '#FFFFFF',
+  },
+  shirtStyle: 'WomensShirt',
+  shirtColor: {
+    name: 'blue',
+    color: '#2674A8',
+  },
+  graphicColor: {
+    color: '#444444',
+    name: 'black',
+  },
+};
 function Shirt() {
-  return React.createElement('div', { className: 'container' }, 'Shirt');
+  // const elt = React.createElement(
+  //   'div',
+  //   null,
+  //   React.createElement('p', null, shirt.name),
+  //   React.createElement('p', null, shirt.description),
+  //   React.createElement('p', null, shirt.price),
+  // );
+  // return elt;
+  return (
+    <div>
+      <p>{shirt.name}</p>
+      <p>{shirt.description}</p>
+      <p>{shirt.price}</p>
+    </div>
+  );
 }
 
 export default Shirt;
