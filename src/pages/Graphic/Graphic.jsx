4f2c5ed1d8fc8bcb0ad500a3f0dd31ee0fa0e505
Hasan Ahmad <hasana@dev6.com>
2020-07-06 00:58:43 -0400

    Added graphic dynamic URL parsing using the useParam hook

--- src/pages/Graphic/Graphic.jsx
+++ src/pages/Graphic/Graphic.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const styles = {
+  greyFont: {
+    color: 'grey',
+  },
+  imgWidth: {
+    width: 180,
+  },
+  graphicHeader: {
+    border: '1px solid black',
+    color: 'grey',
+    textAlign: 'center',
+  },
+  graphicContainer: {
+    border: '1px solid black',
+    backgroundColor: 'grey',
+  },
+};
 
 const Graphic = () => {
-  return <></>;
+  const { graphicLogo } = useParams();
+
+  return (
+    <div>
+      <h3 style={styles.graphicHeader}>Graphic Logo</h3>
+      <img
+        style={styles.graphicContainer}
+        className="img-fluid shirt-graphic-img"
+        src={require(`../../images/shirt-graphics/${graphicLogo}`)}
+        alt="shirt graphic"
+      />
+    </div>
+  );
 };
 
 export default Graphic;
