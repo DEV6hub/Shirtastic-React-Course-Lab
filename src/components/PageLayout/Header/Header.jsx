214818354445795aff530b88125c480c029b2093
Hasan Ahmad <hasana@dev6.com>
2020-07-06 01:11:18 -0400

    Added dynamic routing for Design, added links, and programatic navigation for Header.

--- src/components/PageLayout/Header/Header.jsx
+++ src/components/PageLayout/Header/Header.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import CartControls from '../../CheckoutFlow/CartControls/CartControls';
+import PrimaryButton from '../../PrimaryButton/PrimaryButton';
+import FormInput from '../../Forms/FormInput/FormInput';
 import { useDesignContext } from '../../../state/contexts/designContext';
 import { useShirtsContext } from '../../../state/contexts/shirtsContext';
 import { DESIGN_NAME_EVENT } from '../../../constants/optionEventTypes';
@@ -6,6 +10,9 @@ import './header.css';
 import headerLogo from '../../../images/navlogo.png';
 
 const Header = () => {
+  const location = useLocation();
+  const history = useHistory();
+
   const { shirt, updateShirt, isNewDesign } = useDesignContext();
   const { addShirt, saveShirt } = useShirtsContext();
 
@@ -14,10 +21,47 @@ const Header = () => {
     updateShirt({ type: DESIGN_NAME_EVENT, data: $event.target.value });
   };
 
+  const saveDesign = () => {
+    if (isNewDesign) {
+      addShirt(shirt);
+    } else {
+      saveShirt(shirt);
+    }
+    history.push('/catalog');
+  };
+
   return (
     <header>
-      <div>
-        <img src={headerLogo} className="logo" alt="Shirtastic Logo - By Aquent Dev 6" />
+      <div className="left">
+        <PrimaryButton
+          btnStyle="navigation"
+          onClick={($event) => {
+            $event.preventDefault();
+            history.push('/catalog');
+          }}
+        >
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+        </PrimaryButton>
+        <div className="vertical-line" />
+        <Link to="/">
+          <img src={headerLogo} className="logo" alt="Shirtastic Logo - By Aquent Dev 6" />
+        </Link>
+      </div>
+      <div className="right">
+        {location.pathname.indexOf('design') > -1 ? (
+          <div className="shirt-edit-controls">
+            <FormInput id="design-name" value={shirt.name} onChange={updateName} />
+            <PrimaryButton onClick={saveDesign}>Save</PrimaryButton>
+          </div>
+        ) : (
+          <Link to="/design" className="button button-primary">
+            New Design
+          </Link>
+        )}
+        <div className="vertical-line" />
+        <CartControls />
       </div>
     </header>
   );
--- src/pages/Design/Design.jsx
+++ src/pages/Design/Design.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import PageLayout from '../../components/PageLayout/PageLayout';
 import DesignEditor from '../../components/DesignEditor/DesignEditor';
 import { useDesignContext } from '../../state/contexts/designContext';
+import { useShirtsContext } from '../../state/contexts/shirtsContext';
+
+import initialShirt from '../../constants/initialShirt';
+
+import { SELECT_SHIRT } from '../../constants/optionEventTypes';
 
 const Design = () => {
-  const { shirt, updateShirt } = useDesignContext();
+  const { shirtId } = useParams();
+  const { shirt, updateShirt, setNewDesign } = useDesignContext();
+  const { shirtList } = useShirtsContext();
+
+  useEffect(() => {
+    const initWithNewShirt = () => {
+      updateShirt({ type: SELECT_SHIRT, data: initialShirt });
+      setNewDesign(true);
+    };
+
+    const selectExistingShirt = (existingShirtList, selectedShirtId) => {
+      if (shirtList.length > 0) {
+        const theShirtsList = [...existingShirtList];
+
+        const shirtIndex = theShirtsList.findIndex((shirtItem) => {
+          return shirtItem.id === parseInt(selectedShirtId, 10);
+        });
+
+        if (shirtIndex !== -1) {
+          updateShirt({ type: SELECT_SHIRT, data: theShirtsList[shirtIndex] });
+          setNewDesign(false);
+        } else {
+          initWithNewShirt();
+          setNewDesign(true);
+        }
+      }
+    };
+
+    if (shirtId) {
+      selectExistingShirt(shirtList, shirtId);
+    } else {
+      initWithNewShirt();
+    }
+  }, [setNewDesign, shirtId, shirtList, updateShirt]);
 
   return (
     <PageLayout>
