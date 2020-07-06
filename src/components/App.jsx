53ab59fe5fb7afb0322fe001ba32745b6aafa718
Hasan Ahmad <hasana@dev6.com>
2020-07-06 00:53:13 -0400

    Added Home, Catalog, Design and Graphic routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import AppProvider from '../state/contexts/AppProvider';
+import Home from '../pages/Home/Home';
+import Catalog from '../pages/Catalog/Catalog';
+import Design from '../pages/Design/Design';
+import Graphic from '../pages/Graphic/Graphic';
 
 const App = () => {
   return (
     <AppProvider>
-      <Router></Router>
+      <Router>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/catalog">
+          <Catalog />
+        </Route>
+        <Route exact path="/design/:shirtId?">
+          <Design />
+        </Route>
+        <Route path="/graphic/:graphicLogo">
+          <Graphic />
+        </Route>
+      </Router>
     </AppProvider>
   );
 };
