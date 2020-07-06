c4d3074ded273d643e8c1b63dd03eadaf16659fd
Hasan Ahmad <hasana@dev6.com>
2020-07-06 00:42:39 -0400

    Starting point for lab-4-routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import Home from '../pages/Home/Home';
-import Catalog from '../pages/Catalog/Catalog';
-import Design from '../pages/Design/Design';
-import Graphic from '../pages/Graphic/Graphic';
 import AppProvider from '../state/contexts/AppProvider';
 
 const App = () => {
   return (
     <AppProvider>
-      <Router>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/catalog">
-          <Catalog />
-        </Route>
-        <Route exact path="/design/:shirtId?">
-          <Design />
-        </Route>
-        <Route path="/graphic/:graphicLogo">
-          <Graphic />
-        </Route>
-      </Router>
+      <Router></Router>
     </AppProvider>
   );
 };
--- src/pages/Graphic/Graphic.jsx
+++ src/pages/Graphic/Graphic.jsx
@@ -1,38 +1,7 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-
-const styles = {
-  greyFont: {
-    color: 'grey',
-  },
-  imgWidth: {
-    width: 180,
-  },
-  graphicHeader: {
-    border: '1px solid black',
-    color: 'grey',
-    textAlign: 'center',
-  },
-  graphicContainer: {
-    border: '1px solid black',
-    backgroundColor: 'grey',
-  },
-};
 
 const Graphic = () => {
-  const { graphicLogo } = useParams();
-
-  return (
-    <div>
-      <h3 style={styles.graphicHeader}>Graphic Logo</h3>
-      <img
-        style={styles.graphicContainer}
-        className="img-fluid shirt-graphic-img"
-        src={require(`../../images/shirt-graphics/${graphicLogo}`)}
-        alt="shirt graphic"
-      />
-    </div>
-  );
+  return <></>;
 };
 
 export default Graphic;
