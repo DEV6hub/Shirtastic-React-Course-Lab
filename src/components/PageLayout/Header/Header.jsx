94b0eec5d5c330759a4f226c954aebac997c2393
Hasan Ahmad <hasana@dev6.com>
2020-07-05 21:51:42 -0400

    Added core PageLayout jsx.

--- src/components/PageLayout/Header/Header.jsx
+++ src/components/PageLayout/Header/Header.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import headerLogo from '../../../images/navlogo.png';
 
-const Header = () => <></>;
+const Header = () => (
+  <header>
+    <div>
+      <img src={headerLogo} className="logo" alt="Shirtastic Logo" />
+    </div>
+  </header>
+);
 
 export default Header;
--- src/components/PageLayout/PageLayout.jsx
+++ src/components/PageLayout/PageLayout.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Header from './Header/Header.jsx';
+import Footer from './Footer/Footer.jsx';
 
-const PageLayout = ({ children }) => <></>;
+const PageLayout = ({ children }) => (
+  <>
+    <Header />
+    <div className="content">{children}</div>
+    <Footer />
+  </>
+);
 
 export default PageLayout;
